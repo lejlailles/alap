d90e631cbd4d179c55077a9b15d56fe63bd94fcd
lejlailles <il970322@gmail.com>
2022-05-16 20:21:51 +0200

    alap

--- ajax.js
+++ ajax.js
@@ -1,49 +1,59 @@
-class Ajax {
-  constructor(token) {
-    this.token = token;
-  }
+class MyAjax {
+    constructor(token) {
+        this.token = token;
+    }
 
-  getAjax(apivegpont, callback) {
-    $.ajax({
-      url: apivegpont,
-      type: "GET",
-      success: function (result) {
-        callback(result);
-      },
-    });
-  }
+    getAdat(apiVegpont, tomb, myCallback) {
+        $.ajax({
+            url: apiVegpont,
+            type: "GET",
+            success: function (result) {
+                tomb.splice(0, tomb.length);
+                result.forEach((value) => {
+                    tomb.push(value);
+                });
 
-  postAjax(apivegpont, ujAdat) {
-    $.ajax({
-      headers: { "X-CSRF-TOKEN": this.token },
-      url: apivegpont,
-      type: "POST",
-      data: ujAdat,
-    });
-  }
+                myCallback(tomb);
+            },
+        });
+    }
 
-  deleteAjax(apivegpont, id) {
-    $.ajax({
-      headers: { "X-CSRF-TOKEN": this.token },
-      url: apivegpont + "/" + id,
-      data: this.token,
-      type: "DELETE",
-      success: function (result) {
-        console.log(result);
-      },
-    });
-  }
+    postAdat(apiVegpont, adat) {
+        adat._token = this.token;
+        $.ajax({
+            headers: { "X-CSRF-TOKEN": this.token },
+            url: apiVegpont,
+            type: "POST",
+            data: adat,
+            success: function (result) {
+                console.log(result);
+            },
+        });
+    }
 
-  putAjax(apivegpont, id, ujAdat) {
-    ujAdat._token = this.token;
-    $.ajax({
-      headers: { "X-CSRF-TOKEN": this.token },
-      url: apivegpont + "/" + id,
-      type: "PUT",
-      data: ujAdat,
-      success: function (result) {
-        console.log(result);
-      },
-    });
-  }
+    deleteAdat(apiVegpont, id) {
+        $.ajax({
+            headers: { "X-CSRF-TOKEN": this.token },
+            url: apiVegpont + id,
+            type: "DELETE",
+            data: this.token,
+            success: function (result) {
+                console.log(result);
+            },
+        });
+    }
+
+    putAdat(apiVegpont, id, adat) {
+        adat._token = this.token;
+        $.ajax({
+            headers: { "X-CSRF-TOKEN": this.token },
+            datatype: "JSON",
+            type: "PUT",
+            url: apiVegpont + "/" + id,
+            data: adat,
+            success: function (result) {
+                console.log(result);
+            },
+        });
+    }
 }
